6e03aa0d605c5b5162b548661c6827fbeaaf1b4a
MikeG86 <michaelgrunk@gmail.com>
2023-10-08 15:00:11 -0400

    a

--- MachineVision.py
+++ MachineVision.py
@@ -12,7 +12,7 @@ def set_color_ranges():
 
     # Define a threshold range based on the HSV color
     color1_lower = np.array([hsv_color1[0][0][0] - 10, 100, 100])  # Adjust the -10 to fit your desired range
-    color1_upper = np.array([hsv_color1[0][0][0] + 10, 255, 255])  # Adjust the +10 to fit your desired range
+    color1_upper = np.array([hsv_color1[0][0][0] + 20, 255, 255])  # Adjust the +10 to fit your desired range
 
     # Convert RGB color (255, 0, 51) to HSV
     #rgb_color2 = np.uint8([[[255, 0, 51]]])  # RGB color
@@ -24,8 +24,8 @@ def set_color_ranges():
 
     #color1_lower = np.array([25, 100, 100])
     #color1_upper = np.array([35, 255, 255])
-    color2_lower = np.array([100, 50, 100])
-    color2_upper = np.array([120, 153, 255])
+    color2_lower = np.array([100, 100, 100])
+    color2_upper = np.array([120, 255, 255])
     return color1_lower,color1_upper,color2_lower,color2_upper
 
 def view_image_get_distance(color1_lower,color1_upper,color2_lower,color2_upper,cap):
