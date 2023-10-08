9200e0e4564d653a69331fb12b7465b81e0418e6
MikeG86 <michaelgrunk@gmail.com>
2023-10-08 14:13:45 -0400

    a

--- MinimizeDistance.py
+++ MinimizeDistance.py
@@ -7,3 +7,4 @@ while horizontal_d > 50 and vertical_d > 50:
     capture_device = mv.set_capture(0)
     color1_lower,color1_upper,color2_lower,color2_upper = mv.set_color_ranges()
     horizontal_d,vertical_d = mv.view_image_get_distance(color1_lower,color1_upper,color2_lower,color2_upper,capture_device)
+    print(horizontal_d,vertical_d)
