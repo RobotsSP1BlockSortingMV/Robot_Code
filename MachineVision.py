7ee579c7cb02795d0a14df645cb116ce42b402b8
MikeG86 <michaelgrunk@gmail.com>
2023-10-08 14:05:16 -0400

    update

--- MachineVision.py
+++ MachineVision.py
@@ -66,7 +66,7 @@ def view_image_get_distance(color1_lower,color1_upper,color2_lower,color2_upper,
             cv2.putText(frame, f'Horizontal Distance: {horizontal_distance:.2f} pixels', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             cv2.putText(frame, f'Vertical Distance: {vertical_distance:.2f} pixels', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             if count == 1000:
-                return horizontal_distance
+                return horizontal_distance, vertical_distance
 
         # Show the frame
         cv2.imshow('Object Tracking', frame)
