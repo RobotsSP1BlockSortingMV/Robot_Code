126ca9b96aa3998a542b5af7495b4316d1ceda8c
MikeG86 <michaelgrunk@gmail.com>
2023-10-08 13:27:28 -0400

    Fixed

--- MinimizeDistance.py
+++ MinimizeDistance.py
@@ -1,6 +1,7 @@
-import Movement.py as bs
+import Movement as bs
 import MachineVision as mv
 
-num_threads = 2
-capture_device = mv.set_capture(0)
-print(mv.view_image_get_distance(mv.set_color_ranges(),capture_device))
+distance = 100000
+while distance > 100:
+    capture_device = mv.set_capture(0)
+    distance = mv.view_image_get_distance(mv.set_color_ranges(),capture_device)
