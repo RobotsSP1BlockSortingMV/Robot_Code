a408d170434e15bdbfb90077cec31ea106f96f39
MikeG86 <michaelgrunk@gmail.com>
2023-10-08 13:55:55 -0400

    Horizontaldistance

--- MachineVision.py
+++ MachineVision.py
@@ -61,10 +61,10 @@ def view_image_get_distance(color1_lower,color1_upper,color2_lower,color2_upper,
 
         # Calculate distance between centroids
         if 'centroid1' in locals() and 'centroid2' in locals():
-            distance = np.sqrt((centroid1[0] - centroid2[0])**2 + (centroid1[1] - centroid2[1])**2)
-            cv2.putText(frame, f'Distance: {distance:.2f} pixels', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            horizontal_distance = centroid1[0] - centroid2[0]
+            cv2.putText(frame, f'Horizontal Distance: {horizontal_distance:.2f} pixels', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             if count == 1000:
-                return distance
+                return horizontal_distance
 
         # Show the frame
         cv2.imshow('Object Tracking', frame)
